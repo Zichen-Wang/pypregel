3ad02184b00c83d656475dc8eda674c686c402d7
Zichen Wang <zichen.wang@pku.edu.cn>
2020-04-21 17:20:59 -0400

    init project

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from steuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+
+install_requires = [
+    'mpi4py'
+    'wheel',
+    'setuptools'
+]
+
+
+setup(
+    name='pypregel',
+    version='0.0.1',
+    description='Graph framework',
+    long_descriptioon='README',
+    author='Zichen Wang; Hongxin Chu; Chufeng Gao',
+    install_requires=install_requires,
+)
+
